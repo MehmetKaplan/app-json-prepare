57491aaa5f0713b77c858a40967c159dd611e02b
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2020-07-20 11:50:04 +0630

    Last commit

--- app-json-prepare.js
+++ app-json-prepare.js
@@ -3,22 +3,22 @@ const fs = require("fs");
 const appJsonPrepare = () => {
 
 	// increment build number
-	let c_buildNumber;
+	let buildNumber;
 	try{
-		c_buildNumber = Number(fs.readFileSync('./build-counter.txt').toString().split('\n')[0]) + 1;
+		buildNumber = Number(fs.readFileSync('./build-counter.txt').toString().split('\n')[0]) + 1;
 	} catch (err){
-		c_buildNumber = 1;
+		buildNumber = 1;
 	}
-	if (!c_buildNumber) c_buildNumber = 1;
-	fs.writeFileSync('./build-counter.txt', `${c_buildNumber}`);
+	if (!buildNumber) buildNumber = 1;
+	fs.writeFileSync('./build-counter.txt', `${buildNumber}`);
 
 	// read existing app.json as the base
 	const l_retval = JSON.parse(fs.readFileSync('./app.json').toString());
 
 	// modify new values
-	l_retval.expo.ios.buildNumber = `${c_buildNumber}`;
+	l_retval.expo.ios.buildNumber = `${buildNumber}`;
 	if (!(l_retval.expo.android)) l_retval.expo.android = {};
-	l_retval.expo.android.versionCode = c_buildNumber;
+	l_retval.expo.android.versionCode = buildNumber;
 
 	// write as the new configuration
 	fs.writeFileSync('./app.json', `${JSON.stringify(l_retval, null, '\t')}`);
