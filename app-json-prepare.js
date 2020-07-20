f240371aaaba24047281a2c03f8d92fef0f43ce8
Mehmet Kaplan <mehmet.kaplan@gmail.com>
2020-07-20 12:02:03 +0630

    Last commit

--- app-json-prepare.js
+++ app-json-prepare.js
@@ -13,19 +13,19 @@ const appJsonPrepare = () => {
 	fs.writeFileSync('./build-counter.txt', `${buildNumber}`);
 
 	// read existing app.json as the base
-	const l_retval = JSON.parse(fs.readFileSync('./app.json').toString());
+	const appJson = JSON.parse(fs.readFileSync('./app.json').toString());
 
 	// modify new values
-	l_retval.expo.ios.buildNumber = `${buildNumber}`;
-	if (!(l_retval.expo.android)) l_retval.expo.android = {};
-	l_retval.expo.android.versionCode = buildNumber;
+	appJson.expo.ios.buildNumber = `${buildNumber}`;
+	if (!(appJson.expo.android)) appJson.expo.android = {};
+	appJson.expo.android.versionCode = buildNumber;
 
 	// write as the new configuration
-	fs.writeFileSync('./app.json', `${JSON.stringify(l_retval, null, '\t')}`);
+	fs.writeFileSync('./app.json', `${JSON.stringify(appJson, null, '\t')}`);
 
 	// log data
 	console.log(`\x1b[41m\x1b[33mFinal app.json configuration:\x1b[0m`);
-	console.log(JSON.stringify(l_retval, null, '\t'));
+	console.log(JSON.stringify(appJson, null, '\t'));
 };
 
 appJsonPrepare();
